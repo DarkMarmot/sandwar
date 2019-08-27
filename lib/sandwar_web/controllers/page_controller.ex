fd10e7740c6a138031677d9f5df28ebc34b7c944
Bishoy Boktor <bishoyboktor@gmail.com>
2019-08-26 23:54:52 -0500

    user_name test 3

--- lib/sandwar_web/controllers/page_controller.ex
+++ lib/sandwar_web/controllers/page_controller.ex
@@ -5,14 +5,18 @@ defmodule SandwarWeb.PageController do
 
   def index(conn, _) do
     if conn.assigns.current_user do
-      LiveView.Controller.live_render(conn, SandwarWeb.HomeView, session: %{})
+      LiveView.Controller.live_render(conn, SandwarWeb.HomeView,
+        session: %{user_name: conn.assigns.current_user.name}
+      )
     else
       render(conn, "index.html")
     end
   end
 
   def anon(conn, _) do
-    LiveView.Controller.live_render(conn, SandwarWeb.HomeView, session: %{})
+    LiveView.Controller.live_render(conn, SandwarWeb.HomeView,
+      session: %{user_name: conn.assigns.current_user.name}
+    )
   end
 
   def sign_in(conn, _params) do
